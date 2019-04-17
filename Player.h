34aca2389fbe207b45b288e9bab05faf55e6314d
jacobark <jacobbarker98@hotmail.com>
2019-04-17 14:24:43 -0400

    Worked on Player

--- Player.h
+++ Player.h
@@ -11,7 +11,7 @@ class Player {
         vector<pair<int,string>> hand;
         void putCard(int i, vector<pair<int,string>>& river);
         void pullCardRiver(vector<pair<int,string>> river);
-        void pullCardDeck(vector<pair<int,string>>& deck);
+        void pullCardDeck(Deck& deck);
         void displayHand();
        void turn(Deck deck, vector<pair<int,string>> river, GameRules rules);
     private:
