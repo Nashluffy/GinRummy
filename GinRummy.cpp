1fb344e877ad1f347cc395ab0a57ce0eb9f11e2a
Nash Luffman <nashluffman@gmail.com>
2019-04-18 16:01:00 -0400

    anotha one

--- GinRummy.cpp
+++ GinRummy.cpp
@@ -24,8 +24,6 @@ int main(){
     vector<pair<int,string>> river;
     vector<pair<int,string>> testGameRules;
     vector<pair<int,string>> matchedCards;
-<<<<<<< HEAD
-=======
     testGameRules.push_back(make_pair(1,"Spade"));
     testGameRules.push_back(make_pair(1,"Diamond"));
     testGameRules.push_back(make_pair(1,"Club"));
@@ -37,7 +35,6 @@ int main(){
     testGameRules.push_back(make_pair(10,"Heart"));
     testGameRules.push_back(make_pair(10,"Club"));
     testGameRules.push_back(make_pair(10,"Spade"));
->>>>>>> bed8acbf795bec6fbb584274c8ee872043945b63
 
     Deck deck;
     Player player;
@@ -45,26 +42,7 @@ int main(){
     GameRules rules;
     deck.shuffle();
     player.hand = deck.deal();
-<<<<<<< HEAD
-    //computer.hand = deck.deal();
-    int score;
-
-    computer.hand.push_back(make_pair(13,"Heart"));
-    computer.hand.push_back(make_pair(1,"Spade"));
-    computer.hand.push_back(make_pair(1,"Club"));
-    computer.hand.push_back(make_pair(12,"Diamond"));
-    computer.hand.push_back(make_pair(6,"Heart"));
-    computer.hand.push_back(make_pair(9,"Spade"));
-    computer.hand.push_back(make_pair(11,"Spade"));
-    computer.hand.push_back(make_pair(2,"Diamond"));
-    computer.hand.push_back(make_pair(7,"Spade"));
-    computer.hand.push_back(make_pair(8,"Club"));
-
-
-   // player.turn(river, rules);
-=======
     computer.hand = deck.deal();
->>>>>>> bed8acbf795bec6fbb584274c8ee872043945b63
 
     int score;
     /*Testing for Computers turn
